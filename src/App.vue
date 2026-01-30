97ca556a852df3c99e9b6b286b0d5234579e2765
Llego-250 <solideaze1251@gmail.com>
2026-01-30 22:14:43 +0200

    creating the entry point of the app or the home page

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+<div class="text">
+  <h1>TaskBuddy</h1>
+  <h2>Hello Beautiful User❤️, this is the personal task manager</h2>
+  <h3>welcome dear user!</h3>
+
+</div>
+
+<input type="Text" name="New task" placeholder="new task">
+<button>ADD</button><br/><br/>
+
+</template>
+
+<style>
+body {
+  font-family: Arial, sans-serif;
+  margin: 0px;
+  background: linear-gradient(rgb(14, 10, 111), rgb(20, 209, 211));
+}
+
+
+h1 {
+  color: #f1f1f1;
+  text-align: center;
+}
+h2{
+  color:#fdfdfd;
+  margin: 40px;
+}
+
+input {
+  padding: 8px;
+  margin: 5px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+button {
+  padding: 8px 16px;
+  background-color: #469cf8;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #0056b3;
+}
+</style>
