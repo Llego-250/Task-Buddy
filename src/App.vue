d9e8aa25280997043f9ee6f3755d4025b6523d7f
Llego-250 <solideaze1251@gmail.com>
2026-02-05 00:46:05 +0200

    Adding SearchFilter component above the task cards

--- src/App.vue
+++ src/App.vue
@@ -68,6 +68,8 @@
   </div>
 
   <div class="card-container">
+    <SearchFilter :tasks="tasks" @filtered-tasks="handleFilteredTasks" />
+    
     <div class="card completed">
       <div class="card-title">Completed Tasks</div>
       <div class="task">
