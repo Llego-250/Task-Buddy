4ab3407bd115c433eb1779045c7728171d125cf3
Llego-250 <solideaze1251@gmail.com>
2026-02-13 13:19:11 +0200

    feat: Add a new view for displaying time reports.

---
+++ src/views/ReportsView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="page-container">
+    <div class="view-header">
+      <h2>Time Reports</h2>
+    </div>
+    <TimeReports 
+      :show-reports="true"
+      :tasks="store.filteredTasks || store.tasks" 
+      @hide-reports="$router.push('/')"
+    />
+  </div>
+</template>
+
+<script setup>
+import { useTaskStore } from '../stores/taskStore'
+import TimeReports from '../components/TimeReports.vue'
+
+const store = useTaskStore()
+</script>
+
+<style scoped>
+.page-container {
+  padding: 2rem;
+  max-width: 1200px;
+  margin: 0 auto;
+}
+.view-header {
+  margin-bottom: 2rem;
+  text-align: center;
+}
+</style>
