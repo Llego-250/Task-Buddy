2393783a92de8b7753af06c39dc3d31c2c4cedbb
Llego-250 <solideaze1251@gmail.com>
2026-02-17 23:29:29 +0200

    place User-Dashboard.js to Views package

---
+++ src/views/Dashboard/User.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="main-container">
+        <div class="header">
+            <h1 class="title">Dashboard</h1>
+            <h2 class="title">Welcome to the personal task dashboard❤️</h2>
+
+ 
+        </div>
+    </div>
+    <style>
+        :root{
+            font-family: system-ui, Avenir, Helvetica, Arial, sans-serif;
+            line-height: 1.5;
+            font-weight: 400;
+
+            color-scheme: light dark;
+            color: rgba(255, 255, 255, 0.87);
+            background-color: #242424;
+
+            font-synthesis: none;
+            text-rendering: optimizeLegibility;
+            -webkit-font-smoothing: antialiased;
+            -moz-osx-font-smoothing: grayscale;
+        }
+        :root[data-theme="light"]{
+            --bg-primary: linear-gradient(135deg, #e0f2fe, #b3e5fc);
+            --bg-secondary:rgba(255,255,255, 0.9)
+        }
+    </style>
+</template>
