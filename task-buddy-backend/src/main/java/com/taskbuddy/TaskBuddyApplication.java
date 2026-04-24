9c6b52608663f3ad95d8bd6884a3f505202570eb
Llego-250 <solideaze1251@gmail.com>
2026-04-24 13:10:39 +0200

    Create Spring Boot main application entry point

---
+++ task-buddy-backend/src/main/java/com/taskbuddy/TaskBuddyApplication.java
@@ -0,0 +1,11 @@
+package com.taskbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskBuddyApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TaskBuddyApplication.class, args);
+    }
+}
