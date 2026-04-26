8dad08f6e042612fa8c8dd2562c743abe5d902a8
Llego-250 <solideaze1251@gmail.com>
2026-04-26 15:37:28 +0200

    Remove Lombok from AnalyticsController

--- task-buddy-backend/src/main/java/com/taskbuddy/controller/AnalyticsController.java
+++ task-buddy-backend/src/main/java/com/taskbuddy/controller/AnalyticsController.java
@@ -2,17 +2,19 @@ package com.taskbuddy.controller;
 
 import com.taskbuddy.dto.AnalyticsResponse;
 import com.taskbuddy.service.AnalyticsService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/analytics")
-@RequiredArgsConstructor
 public class AnalyticsController {
 
     private final AnalyticsService analyticsService;
+
+    public AnalyticsController(AnalyticsService analyticsService) {
+        this.analyticsService = analyticsService;
+    }
 
     @GetMapping("/summary")
     public AnalyticsResponse getSummary() {
